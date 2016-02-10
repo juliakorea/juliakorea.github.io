7ea904bafb82b20b979f4ec2efee0ed571d1e3d6
wookyoung <wookay.noh@gmail.com>
2016-02-11 01:02:56 +0900

    번역 안된 문서도 표시 apply https://github.com/juliakorea/doc/pull/44

---
+++ latest/_static/blame.js
@@ -0,0 +1,9 @@
+function blame(pagename) {
+  var translated = ["index", "manual/complex-and-rational-numbers", "manual/getting-started", "manual/integers-and-floating-point-numbers", "manual/introduction", "manual/mathematical-operations", "manual/variables", "phdthesis/chap6"];
+  if (-1 == $.inArray(pagename, translated)) {
+    window.open("https://github.com/JuliaLang/julia/blame/master/doc/" + pagename + ".rst", "blame");
+  } else {
+    window.open("https://github.com/juliakorea/doc/blame/master/src/julia/doc/" + pagename + ".rst.txt", "blame");
+  }
+}
+
